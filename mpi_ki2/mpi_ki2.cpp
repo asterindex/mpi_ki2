e635ec9c90977773f12309f93b968e6d2aa7d0d4
asterindex <11225894+asterindex@users.noreply.github.com>
2022-10-25 13:49:39 +0300

    Add project files.

---
+++ mpi_ki2/mpi_ki2.cpp
@@ -0,0 +1,11 @@
+﻿// mpi_ki2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "mpi.h"
+
+
+int main(int argc, char** argv)
+{
+    
+}
