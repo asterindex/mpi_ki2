a80a2d55f51c087a7b6507c3168e6773f972cf72
asterindex <11225894+asterindex@users.noreply.github.com>
2022-10-25 14:52:03 +0300

    broadcast

--- mpi_ki2/mpi_ki2.cpp
+++ mpi_ki2/mpi_ki2.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char** argv)
 {
-    int ProcNum, ProcRank, RecvRank;
+    /*int ProcNum, ProcRank, RecvRank;
     MPI_Status Status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
@@ -28,5 +28,50 @@ int main(int argc, char** argv)
         MPI_Send(&test, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
     MPI_Finalize();
+    return 0;*/
+
+    double x[100], TotalSum, ProcSum = 0.0;
+    int ProcRank, ProcNum, N = 100;
+    MPI_Status Status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    // data preparation
+    if (ProcRank == 0)
+    {
+        for (int i = 0; i < 100; i++)
+            x[i] = 1;
+    }
+
+    // broadcast to all
+   
+    MPI_Bcast(x, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+   
+    // partial sum calculation of a vector from i1 to i2
+    int k = N / ProcNum;
+    int i1 = k * ProcRank;
+    int i2 = k * (ProcRank + 1);
+    if (ProcRank == ProcNum - 1) i2 = N;
+    for (int i = i1; i < i2; i++)
+        ProcSum = ProcSum + x[i];
+    
+    // assembling partial sum on process 0 
+    if (ProcRank == 0)
+    {
+        TotalSum = ProcSum;
+        for (int i = 1; i < ProcNum; i++) {
+            MPI_Recv(&ProcSum, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &Status);
+            TotalSum = TotalSum + ProcSum;
+        }
+    }
+    else // all process send the partial sums
+        MPI_Send(&ProcSum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    // print result
+    if (ProcRank == 0)
+        printf("\nTotal Sum = %10.2f", TotalSum);
+
+    MPI_Finalize();
+
     return 0;
+
 }
